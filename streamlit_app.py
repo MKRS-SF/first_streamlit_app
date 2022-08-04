0f6908c4ca73c10c4158ac76bb75f7bbc6c8ca68
MKRS-SF <110589561+MKRS-SF@users.noreply.github.com>
2022-08-04 12:50:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,7 @@ streamlit.text('Avacado Toast')
 
 
 streamlit.header('\N{watermelon}\N{banana}Build your own Fruit Smoothie\N{watermelon}\N{banana}')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
